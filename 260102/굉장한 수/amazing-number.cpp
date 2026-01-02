66277a408707e9076d5e444582221a820c4a0f52
mminjooo <mminjooo09@gmail.com>
2026-01-02 17:17:13 +0900

    Title: 굉장한 수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 260102/굉장한 수/amazing-number.cpp
+++ 260102/굉장한 수/amazing-number.cpp
@@ -6,10 +6,7 @@ int main() {
 
     std::cin >> N;
 
-    if (N%2==1 && N%3==0) {
-        std::cout << "true";
-    }
-    else if (N%2==0 && N%5==0) {
+    if ((N%2==1 && N%3==0) || (N%2==0 && N%5==0)) {
         std::cout << "true";
     }
     else {
