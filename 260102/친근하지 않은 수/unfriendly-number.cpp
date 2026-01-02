44a03f0e12f1b442d49b15893a212bf4c574116a
mminjooo <mminjooo09@gmail.com>
2026-01-02 19:54:11 +0900

    Title: 친근하지 않은 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260102/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    int count = 0;
+    std::cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        if ((i%2==0) || (i%3==0) || (i%5==0)) {
+            continue;
+        }
+
+        count += 1;
+    }
+    std::cout << count;
+    
+    return 0;
+}
