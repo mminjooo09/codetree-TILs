33ca5df1f3c085a7ca871389ca20c93b3cbbb921
mminjooo <mminjooo09@gmail.com>
2026-01-02 16:31:24 +0900

    Title: 문자 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260102/문자 출력하기/print-text.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a;
+
+    std::cin >> a;
+
+    for (int i = 0; i < 8; i++) {
+        std::cout << a;
+    }
+
+    return 0;
+}
