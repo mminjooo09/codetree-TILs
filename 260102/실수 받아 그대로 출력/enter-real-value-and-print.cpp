051441dde9581625d023a5d837293c2e8197f873
mminjooo <mminjooo09@gmail.com>
2026-01-02 15:34:57 +0900

    Title: 실수 받아 그대로 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260102/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double N;
+
+    std::cin >> N;
+
+    std::cout << fixed;
+    std::cout.precision(2);
+    std::cout << N;
+    
+    return 0;
+}
