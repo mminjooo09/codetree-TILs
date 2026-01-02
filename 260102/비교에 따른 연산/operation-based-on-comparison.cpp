dd8e03e08f7f37b71322cb93a90dbaa00de99afc
mminjooo <mminjooo09@gmail.com>
2026-01-02 16:17:31 +0900

    Title: 비교에 따른 연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260102/비교에 따른 연산/operation-based-on-comparison.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    std::cin >> a >> b;
+
+    if (a > b) {
+        std::cout << a*b;
+    }
+    else {
+        std::cout << b/a;
+    }
+
+    return 0;
+}
