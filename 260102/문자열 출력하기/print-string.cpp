394d155790eb7af880b8155026de1af32d9f0fa4
mminjooo <mminjooo09@gmail.com>
2026-01-02 16:34:02 +0900

    Title: 문자열 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260102/문자열 출력하기/print-string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+
+    std::cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        std::cout << "LeebrosCode" << endl;
+    }
+
+    return 0;
+}
