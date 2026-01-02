084a7278d3923ae7c6e15a126511ce3c85cb5e26
mminjooo <mminjooo09@gmail.com>
2026-01-02 18:01:26 +0900

    Title: 제곱하여 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260102/제곱하여 출력하기/print-square-of-elements.cpp
+++ 260102/제곱하여 출력하기/print-square-of-elements.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main() {
     int N;
+    std::cin >> N;
+
     int array[N];
 
-    std::cin >> N;
-    
     for (int i = 0; i < N; i++) {
         std::cin >> array[i];
     }
