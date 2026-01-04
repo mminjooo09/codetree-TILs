1dae61af4699d24048485b4a98c4c54f50e836b5
mminjooo <mminjooo09@gmail.com>
2026-01-04 23:21:00 +0900

    Title: 특정 원소들의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260104/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[4][4];
+    int sum = 0;
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j <= i; j++) {
+            sum += arr[i][j];
+        }
+    }
+
+    std::cout << sum;
+
+    return 0;
+}
