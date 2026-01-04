ec1779f132129155310f9268767da044ffedadb3
mminjooo <mminjooo09@gmail.com>
2026-01-04 22:22:24 +0900

    Title: 약수가 세 개인 수, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260104/약수가 세 개인 수/numbers-with-three-divisors.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int st, ed;
+    std::cin >> st >> ed;
+    int result = 0;
+
+    for (int i = st; i <= ed; i++) {
+        int cnt = 0;
+
+        for (int j = 1; j <= i; j++) {
+            if (i % j == 0) {
+                cnt++;
+            }
+        }
+
+        if (cnt == 3) {
+            result++;
+        }
+    }
+
+    std::cout << result;
+
+    return 0;
+}
