7dd7a4301b14f9a81ba76a84dc76edb8c57045a9
mminjooo <mminjooo09@gmail.com>
2026-01-04 23:45:02 +0900

    Title: 좌우로 반복하며 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260104/좌우로 반복하며 출력/print-in-left-right-reverse.cpp
+++ 260104/좌우로 반복하며 출력/print-in-left-right-reverse.cpp
@@ -4,30 +4,18 @@ using namespace std;
 int main() {
     int N;
     std::cin >> N;
-    int arr[N][N];
 
     for (int i = 0; i < N; i++) {
-        int cnt = 1;
-
-        if (i % 2 == 0) {
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = cnt;
-                cnt++;
-            }
-        }
-        else {
-            for (int j = N-1; j >= 0; j--) {
-                arr[i][j] = cnt;
-                cnt++;
-            }
-        }
-
         for (int j = 0; j < N; j++) {
-            std::cout << arr[i][j];
+            if (i % 2 == 0) {
+                std::cout << j + 1;
+            }
+            else {
+                std::cout << N - j;
+            }
         }
-
         std::cout << endl;
     }
-
+    
     return 0;
 }
