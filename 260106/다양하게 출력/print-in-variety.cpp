369a1641f22823fb948aa93374613616905c99e2
mminjooo <mminjooo09@gmail.com>
2026-01-06 13:51:13 +0900

    Title: 다양하게 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260106/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "Total days in Year" << endl;
+    std::cout << 365 << endl;
+    std::cout << "Circumference rate" << endl;
+    std::cout << "3.1415926535";
+
+    return 0;
+}
