45a2da1680af38fe66145b6907e807dfbc01233e
mminjooo <mminjooo09@gmail.com>
2026-01-07 14:56:57 +0900

    Title: 입력받아 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260107/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    std::cin >> a;
+    std::cin.get();
+    std::cin >> b;
+
+    std::cout << a << b;
+    
+    return 0;
+}
