b71232b338ab246330e73edfd897f137151b01b0
mminjooo <mminjooo09@gmail.com>
2026-01-07 14:44:42 +0900

    Title: 세 실수의 반올림, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260107/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b, c;
+    std::cin >> a >> b >> c;
+
+    std::cout << fixed;
+    std::cout.precision(3);
+    
+    std::cout << a << endl << b << endl << c;
+    
+    return 0;
+}
