29b14b1f53f03b09837e014740c26cfafa617807
mminjooo <mminjooo09@gmail.com>
2026-01-07 15:09:53 +0900

    Title: 간단한 사칙연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260107/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    std::cin >> A >> B;
+
+    std::cout << A+B << endl << A-B << endl << A/B << endl << A%B;
+    
+    return 0;
+}
