44345f208076fa30a6a0a0f1d78e88d4607a25cd
mminjooo <mminjooo09@gmail.com>
2026-01-07 14:55:57 +0900

    Title: 날짜 변경하여 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260107/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, d ,y;
+    char a;
+    std::cin >> m >> a >> d >> a >> y;
+
+    std::cout << y << "." << m << "." << d;
+    
+    return 0;
+}
