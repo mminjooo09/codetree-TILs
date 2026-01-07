af7c6538bf39ca59db5a7e01f680b00de989adbf
mminjooo <mminjooo09@gmail.com>
2026-01-07 15:05:45 +0900

    Title: 전화번호 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260107/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    char h;
+    std::cin >> a >> h >> b >> h >> c;
+
+    std::cout << 0 << a << h << c << h << b;
+
+    return 0;
+}
