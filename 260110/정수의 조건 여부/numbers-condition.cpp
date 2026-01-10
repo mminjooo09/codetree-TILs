0c07a9d5e0ff9511203a6ad8a322d3cb574ba346
mminjooo <mminjooo09@gmail.com>
2026-01-10 14:21:51 +0900

    Title: 정수의 조건 여부, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260110/정수의 조건 여부/numbers-condition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    std::cin >> a;
+
+    if (a >= 113)
+        std::cout << 1;
+    else
+        std::cout << 0;
+    
+    return 0;
+}
