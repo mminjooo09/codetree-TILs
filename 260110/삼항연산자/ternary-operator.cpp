efe37f7cf3621ab36c8e3b320e1ab3d44c7fdbb9
mminjooo <mminjooo09@gmail.com>
2026-01-10 14:28:31 +0900

    Title: 삼항연산자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260110/삼항연산자/ternary-operator.cpp
+++ 260110/삼항연산자/ternary-operator.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
     int score;
     std::cin >> score;
-    string n;
 
-    n = score == 100 ? "pass" : "failure";
+    std::cout << (score == 100 ? "pass" : "failure");
 
-    std::cout << n;
-    
     return 0;
 }
