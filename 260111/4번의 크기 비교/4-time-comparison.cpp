dadd0e2d2fd8efab5a64fa0283167cc26626eaee
mminjooo <mminjooo09@gmail.com>
2026-01-11 16:59:10 +0900

    Title: 4번의 크기 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260111/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C, D, E;
+    std::cin >> A >> B >> C >> D >> E;
+
+    std::cout << (A>B) << endl;
+    std::cout << (A>C) << endl;
+    std::cout << (A>D) << endl;
+    std::cout << (A>E) << endl;
+    
+    return 0;
+}
