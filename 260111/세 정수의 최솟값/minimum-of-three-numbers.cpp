a42602f5240ab4127b6733c36f2eee620f14e836
mminjooo <mminjooo09@gmail.com>
2026-01-11 17:18:04 +0900

    Title: 세 정수의 최솟값, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260111/세 정수의 최솟값/minimum-of-three-numbers.cpp
+++ 260111/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -5,7 +5,7 @@ int main() {
     int a, b, c;
     std::cin >> a >> b >> c;
 
-    if ((a<b) && (a<c))
+    if ((a<=b) && (a<=c))
         std::cout << a;
     else if ((b<=a) && (b<=c))
         std::cout << b;
