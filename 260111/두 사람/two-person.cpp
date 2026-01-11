a4b6e226ec3800a259d86e140fb6e28ee2d39aee
mminjooo <mminjooo09@gmail.com>
2026-01-11 17:53:33 +0900

    Title: 두 사람, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260111/두 사람/two-person.cpp
+++ 260111/두 사람/two-person.cpp
@@ -6,10 +6,12 @@ int main() {
     char a_sex, b_sex;
     std::cin >> a_age >> a_sex >> b_age >> b_sex;
 
-    if ((a_age < 19) || (b_age < 19))
-        std::cout << 0;
-    else if ((a_sex == 'M') || (b_sex == 'M'))
+    if ((a_age >= 19) && (a_sex == 'M'))
         std::cout << 1;
+    else if ((b_age >= 19) && (b_sex == 'M'))
+        std::cout << 1;
+    else
+        std::cout << 0;
 
     return 0;
 }
