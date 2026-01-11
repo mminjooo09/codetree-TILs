931efaa0eb21b7ee833346c6aa03088c49863aec
mminjooo <mminjooo09@gmail.com>
2026-01-11 16:57:43 +0900

    Title: 4가지 관계연산자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260111/4가지 관계연산자/four-relational-operators.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    std::cin >> A >> B;
+
+    std::cout << (A>=B) << endl;
+    std::cout << (A> B) << endl;
+    std::cout << (A<=B) << endl;
+    std::cout << (A< B) << endl;
+
+    return 0;
+}
