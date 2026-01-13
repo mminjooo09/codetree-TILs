f2852579a735667b0348c250be147a9b3f457fcd
mminjooo <mminjooo09@gmail.com>
2026-01-13 17:41:56 +0900

    Title: 일 수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260113/일 수 구하기/number-of-days-in-month.cpp
+++ 260113/일 수 구하기/number-of-days-in-month.cpp
@@ -7,10 +7,16 @@ int main() {
 
     if (n == 2)
         std::cout << 28;
-    else if (n % 2 == 0)
-        std::cout << 30;
+    else if (n <= 7)
+        if (n % 2 == 0)
+            std::cout << 30;
+        else
+            std::cout << 31;
     else
-        std::cout << 31;
-
+        if (n % 2 == 0)
+            std::cout << 31;
+        else
+            std::cout << 30;
+        
     return 0;
 }
