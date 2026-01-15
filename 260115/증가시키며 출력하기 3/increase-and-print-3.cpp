2c626081cc90e6dd3e8e4566181e676122f43599
mminjooo <mminjooo09@gmail.com>
2026-01-15 16:46:53 +0900

    Title: 증가시키며 출력하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260115/증가시키며 출력하기 3/increase-and-print-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cnt = 10;
+
+    while (cnt<=26) {
+        std::cout << cnt << " ";
+        cnt += 2;
+    }
+    
+    return 0;
+}
