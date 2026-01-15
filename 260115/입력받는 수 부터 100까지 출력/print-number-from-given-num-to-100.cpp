a858bf2af87509059cc907b273d2ed7521439d7e
mminjooo <mminjooo09@gmail.com>
2026-01-15 16:26:59 +0900

    Title: 입력받는 수 부터 100까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260115/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    for (int i=n; i<101; i++) {
+        std::cout << i << " ";
+    }
+    
+    return 0;
+}
