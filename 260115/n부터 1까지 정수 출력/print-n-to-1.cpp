9ff22d33a073797eed8dd6b583ef6a606ad3bd48
mminjooo <mminjooo09@gmail.com>
2026-01-15 16:57:40 +0900

    Title: n부터 1까지 정수 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260115/n부터 1까지 정수 출력/print-n-to-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    while (n>=1) {
+        std::cout << n << " ";
+        n--;
+    }
+
+    return 0;
+}
