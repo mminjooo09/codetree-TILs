114347d73e23ed613f0cb53bea43c340b2cf2edc
mminjooo <mminjooo09@gmail.com>
2026-01-15 16:55:13 +0900

    Title: 짝수만 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260115/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    std::cin >> A >> B;
+
+    while (A<=B) {
+        if (A%2==0)
+            std::cout << A << " ";
+        A++;
+    }
+    
+    return 0;
+}
