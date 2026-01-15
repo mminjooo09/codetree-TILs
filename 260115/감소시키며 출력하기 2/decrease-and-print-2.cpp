ef575bf673370d83d762b91983c7d029b5c041e5
mminjooo <mminjooo09@gmail.com>
2026-01-15 16:56:36 +0900

    Title: 감소시키며 출력하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260115/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cnt = 26;
+
+    while (cnt>=10) {
+        std::cout << cnt << " ";
+        cnt--;
+    }
+    
+    return 0;
+}
