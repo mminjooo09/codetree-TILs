9f20f748181a798b94d0d7a2036f9d4e95cf380b
mminjooo <mminjooo09@gmail.com>
2026-01-15 16:32:31 +0900

    Title: 1부터 n까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260115/1부터 n까지 출력/print-from-1-to-n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    std::cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        std::cout << i << " ";
+    }
+
+    return 0;
+}
