e2446d488413621ca6bd23cf80274ecbea7af638
mminjooo <mminjooo09@gmail.com>
2026-01-16 17:04:17 +0900

    Title: 등장횟수 확인, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260116/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int cnt = 0;
+
+    for (int i=0; i<5; i++) {
+        std::cin >> a;
+        if (a%2==0)
+            cnt++;
+    }
+
+    std::cout << cnt;
+
+    return 0;
+}
