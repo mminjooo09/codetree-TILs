d19a6baefb1efe43a130e84441468b9fbf959751
mminjooo <mminjooo09@gmail.com>
2026-01-16 16:17:09 +0900

    Title: 100점 도달하기, Time: 1083ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 260116/100점 도달하기/reach-100-point.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    std::cin >> N;
+
+    while (N<=100) {
+        if (N>=90)
+            std::cout << "A ";
+        if (N>=90)
+            std::cout << "A ";
+        if (N>=90)
+            std::cout << "A ";
+        if (N>=90)
+            std::cout << "A ";
+    }
+    return 0;
+}
