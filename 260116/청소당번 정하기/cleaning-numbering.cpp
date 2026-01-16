bef6b31dcb5c0332a3927c68e6c7848d75f4d05e
mminjooo <mminjooo09@gmail.com>
2026-01-16 16:59:04 +0900

    Title: 청소당번 정하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260116/청소당번 정하기/cleaning-numbering.cpp
+++ 260116/청소당번 정하기/cleaning-numbering.cpp
@@ -6,10 +6,8 @@ int main() {
     int cnt2=0, cnt3=0, cnt12=0;
     std::cin >> n;
 
-    for (int i=1; i<n; i++) {
-        if ((i%2==0) && (i%3==0) && (i%12==0))
-            cnt12++;
-        else if (i%12==0)
+    for (int i=1; i<=n; i++) {
+        if (i%12==0)
             cnt12++;
         else if (i%3==0)
             cnt3++;
