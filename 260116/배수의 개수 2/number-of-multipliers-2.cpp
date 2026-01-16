8bd56f33dda3ee3dea083f66747214a706c6ef04
mminjooo <mminjooo09@gmail.com>
2026-01-16 16:49:14 +0900

    Title: 배수의 개수 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260116/배수의 개수 2/number-of-multipliers-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int cnt = 0;
+
+    for (int i=0; i<10; i++) {
+        std::cin >> a;
+
+        if (a%2==1)
+            cnt++;
+    }
+
+    std::cout << cnt;
+
+    return 0;
+}
