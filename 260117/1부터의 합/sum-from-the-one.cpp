f60c593c913dd78d3e0e17a77bcd5bd68922c48d
mminjooo <mminjooo09@gmail.com>
2026-01-17 16:38:41 +0900

    Title: 1부터의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260117/1부터의 합/sum-from-the-one.cpp
+++ 260117/1부터의 합/sum-from-the-one.cpp
@@ -3,17 +3,16 @@ using namespace std;
 
 int main() {
     int N;
-    int M = 0;
+    int sum = 0;
     std::cin >> N;
 
-    for (int i=1; i<=101; i++) {
-        if (M<N)
-           M += i;
-        else {
-            std::cout << i-1;
+    for (int i=1; i<=100; i++) {
+        sum += i;
+        
+        if (sum >= N) {
+            std::cout << i;
             break;
         }
-        
     }
 
     return 0;
