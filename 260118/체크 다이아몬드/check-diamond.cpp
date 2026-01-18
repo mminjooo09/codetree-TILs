e391a11ab3a735ac271ab5bde7c8b6a75ba4658f
mminjooo <mminjooo09@gmail.com>
2026-01-18 14:21:23 +0900

    Title: 체크 다이아몬드, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260118/체크 다이아몬드/check-diamond.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    std::cin >> N;
+
+    for (int i=1; i<=N; i++) {
+        for (int j=1; j<=N-i; j++) {
+            std::cout << " ";
+        }
+        for (int j=1; j<=i; j++) {
+            std::cout << "* ";
+        }
+        std::cout << endl;
+    }
+
+    for (int i=1; i<=N; i++) {
+        for (int j=1; j<=i; j++) {
+            std::cout << " ";
+        }
+        for (int j=1; j<=N-i; j++) {
+            std::cout << "* ";
+        }
+        std::cout << endl;
+    }
+
+    return 0;
+}
