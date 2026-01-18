57d1f18b5f0cd5cf0707cbcd65484d0e8e1493bf
mminjooo <mminjooo09@gmail.com>
2026-01-18 15:22:05 +0900

    Title: 별표 출력하기 13, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260118/별표 출력하기 13/print-start-13.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    std::cin >> N;
+
+    for (int i=1; i<=2*N; i++) {
+        if (i%2==0) {
+            for (int j=1; j<=i/2; j++) {
+                std::cout << "* ";
+            }
+        }
+        else {
+            for (int j=1; j<=(2*N-i)/2+1; j++) {
+                std::cout << "* ";
+            }
+        }
+        
+        std:;cout << endl;
+    }
+
+    return 0;
+}
