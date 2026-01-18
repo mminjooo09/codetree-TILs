845608379aa7c37da06f01441a744c65c78c18e8
mminjooo <mminjooo09@gmail.com>
2026-01-18 13:10:11 +0900

    Title: 별표 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260118/별표 출력하기/print-star.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    std::cin >> N;
+
+    for (int i=1; i<=N; i++) {
+        for (int j=1; j<=i; j++) {
+            std::cout << "* ";
+        }
+        std::cout << endl;
+    }
+
+    for (int i=N-1; i>0; i--) {
+        for (int j=i; j>0; j--) {
+            std::cout << "* ";
+        }
+        std::cout << endl;
+    }
+
+    return 0;
+}
