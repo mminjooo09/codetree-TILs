6e0fc4798fc1e9e049d368c00a2b79fb4d5db213
mminjooo <mminjooo09@gmail.com>
2026-01-18 14:13:03 +0900

    Title: 별 그리기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260118/별 그리기/star-drawing.cpp
+++ 260118/별 그리기/star-drawing.cpp
@@ -5,21 +5,21 @@ int main() {
     int N;
     std::cin >> N;
 
-    for (int i=0; i<N; i++) {
-        for (int j=0; j<N-i-1; j++) {
+    for (int i=1; i<=N; i++) {
+        for (int j=1; j<=N-i; j++) {
             std::cout << " ";
         }
-        for (int j=0; j<=2*i; j++) {
+        for (int j=1; j<=2*i-1; j++) {
             std::cout << "*";
         }
         std::cout << endl;
     }
 
-    for (int i=1; i<N; i++) {
-        for (int j=0; j<i; j++) {
+    for (int i=N-1; i>0; i--) {
+        for (int j=1; j<=N-i; j++) {
             std::cout << " ";
         }
-        for (int j=0; j<=2*(N-i-1); j++) {
+        for (int j=1; j<=2*i-1; j++) {
             std::cout << "*";
         }
         std::cout << endl;
