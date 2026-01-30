34f7ffbdc7fa83d9274523bf75b78efe43492b1f
mminjooo <mminjooo09@gmail.com>
2026-01-30 18:36:03 +0900

    Title: 알파벳 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260130/알파벳 출력하기/print-alphabet.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char cnt = 'A';
+    int N;
+    std::cin >> N;
+
+    for (int i=1; i<=N; i++) {
+        for (int j=1; j<=i; j++) {
+            std::cout << cnt;
+            if (cnt == 'Z')
+                cnt = 'A';
+            else
+                cnt++;
+        }
+        std::cout << endl;
+    }
+
+    return 0;
+}
