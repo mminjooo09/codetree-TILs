db0f743a4bbd4b1ce94e644b7146903e42f6bffe
mminjooo <mminjooo09@gmail.com>
2026-02-03 21:21:35 +0900

    Title: N개의 정수 중 최대 2개, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 260203/N개의 정수 중 최대 2개/two-max-of-n-num.cpp
+++ 260203/N개의 정수 중 최대 2개/two-max-of-n-num.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
-#include <climits>
 using namespace std;
 
 int main() {
     int N;
     std::cin >> N;
+
     int arr[N];
-    int idx;
-
-    int max1=INT_MIN;
-    for (int i=0; i<N; i++) {
+    for (int i=0; i<N; i++)
         std::cin >> arr[i];
-
-        if (arr[i] >= max1) {
-            max1 = arr[i];
-            idx=i;
-        }
+    
+    int max1, max2;
+    if (arr[0]>arr[1]) {
+        max1 = arr[0];
+        max2 = arr[1];
+    }
+    else {
+        max1 = arr[1];
+        max2 = arr[0];
     }
 
-    int max2=INT_MIN;
-    arr[idx] = INT_MIN;
-    for (int i=0; i<N; i++) {
-        if (arr[i] >= max2) {
+    for (int i=2; i<N; i++) {
+        if (arr[i] >= max1) {
+            max2 = max1;
+            max1 = arr[i];
+        }
+        else if (arr[i] > max2) {
             max2 = arr[i];
         }
     }
