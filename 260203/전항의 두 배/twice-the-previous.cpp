2bb78847a56bc5fcc5c43766d3e9632105c0c71f
mminjooo <mminjooo09@gmail.com>
2026-02-03 16:45:39 +0900

    Title: 전항의 두 배, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260203/전항의 두 배/twice-the-previous.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    std::cin >> arr[0] >> arr[1];
+    std::cout << arr[0] << " " << arr[1] << " ";
+
+    for (int i=2; i<10; i++) {
+        arr[i] = arr[i-1] + 2*arr[i-2];
+        
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+}
