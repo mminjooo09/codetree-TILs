e79911cea1335bba2bdee8bca5277b869a1c5a6e
mminjooo <mminjooo09@gmail.com>
2026-02-03 18:56:19 +0900

    Title: 나눗셈의 나머지, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260203/나눗셈의 나머지/remainder-of-division.cpp
+++ 260203/나눗셈의 나머지/remainder-of-division.cpp
@@ -4,26 +4,19 @@ using namespace std;
 int main() {
     int A, B;
     std::cin >> A >> B;
-    int R=0; // R:나머지
-    int cnt[10]={};
-    int rlt=0;
+    int cnt[10] = {};
+    int ans = 0;
 
-    for (int i=0; i<100; i++) {
-        R = A%B;
-        A = A/B;
-
-        cnt[R]++;
-
-        if (A<=1)
-            break;
+    while (A>1) {
+        cnt[A%B]++;
+        A /= B;
     }
 
     for (int i=0; i<10; i++) {
-        cnt[i] *= cnt[i];
-        rlt += cnt[i];
+        ans += cnt[i] * cnt[i];
     }
 
-    std::cout << rlt;
-    
+    std::cout << ans;
+
     return 0;
 }
