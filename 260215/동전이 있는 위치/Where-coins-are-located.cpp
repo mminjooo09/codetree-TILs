95b5a66d0118755bec07de13248b3b2f1cc12074
mminjooo <mminjooo09@gmail.com>
2026-02-15 14:13:38 +0900

    Title: 동전이 있는 위치, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260215/동전이 있는 위치/Where-coins-are-located.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, M;
+    std::cin >> N >> M;
+
+    int arr[N][N] = {};
+    for (int i=0; i<M; i++) {
+        int x, y;
+        std::cin >> x >> y;
+
+        arr[x-1][y-1] = 1;
+    }
+
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N; j++)
+            std::cout << arr[i][j] << " ";
+        
+        std::cout << endl;
+    }
+
+    return 0;
+}
