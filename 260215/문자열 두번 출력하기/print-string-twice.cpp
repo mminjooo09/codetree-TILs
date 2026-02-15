9a49d64778a7bc3197541f5a5b1b6e994a643365
mminjooo <mminjooo09@gmail.com>
2026-02-15 14:47:59 +0900

    Title: 문자열 두번 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260215/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    
+    std::cin >> str;
+    std::cout << str << endl << str;
+    
+    return 0;
+}
