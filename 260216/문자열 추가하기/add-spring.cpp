8e3be3fd076b06931d06f47923692a5bae11ba10
mminjooo <mminjooo09@gmail.com>
2026-02-16 16:38:09 +0900

    Title: 문자열 추가하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260216/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string input;
+    std::cin >> input;
+
+    string output;
+    output = input + "Hello";
+    std::cout << output;
+    
+    return 0;
+}
