3187b142dcbfaffe2eaf28682f05a58fc58864b2
mminjooo <mminjooo09@gmail.com>
2026-02-18 17:31:41 +0900

    Title: 아스키코드 변환, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260218/아스키코드 변환/convert-to-ascii.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char a;
+    int b;
+    std::cin >> a >> b;
+
+    std::cout << (int)a << " " << (char)b;
+    return 0;
+}
