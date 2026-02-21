42abd15580cafb77af69b28bc385f83f0f8b0389
mminjooo <mminjooo09@gmail.com>
2026-02-21 15:50:26 +0900

    Title: 함수를 이용한 369 게임, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

--- 260221/함수를 이용한 369 게임/369-games-using-functions.cpp
+++ 260221/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -5,16 +5,18 @@ bool ThreeMultiple(int i) {
     return i%3==0;
 }
 
-bool ThreeInTen(int i) {
-    return i/10==3 || i/10==6 || i/10==9;
-}
-
-bool ThreeInOne(int i) {
-    return i%10==3 || i%10==6 || i%10==9;
-}
-
 bool ThreeSixNine(int i) {
-    return ThreeInOne(i) || ThreeInTen(i) || ThreeMultiple(i);
+    while (i>0) {
+        if (i%10==3 || i%10==6 || i%10==9)
+            return true;
+        i /= 10;
+    }
+    
+    return false;
+}
+
+bool Has369(int i) {
+    return ThreeSixNine(i) || ThreeMultiple(i);
 }
 
 int main() {
@@ -23,7 +25,7 @@ int main() {
 
     int cnt = 0;
     for (int i=A; i<=B; i++) {
-        if (ThreeSixNine(i))
+        if (Has369(i))
             cnt++;
     }
 
