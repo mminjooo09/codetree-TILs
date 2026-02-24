4c6a4a449e19f4547eb825e74f342e1ab3e12a06
mminjooo <mminjooo09@gmail.com>
2026-02-24 15:46:45 +0900

    Title: 2021년 날짜의 유무, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260224/2021년 날짜의 유무/with-or-without-2021.cpp
+++ 260224/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -1,43 +1,19 @@
 #include <iostream>
 using namespace std;
 
-bool Day24 (int d) {
-    if (1<=d && d<=28)
-        return true;
+int LastDayNum(int m) {
+    if (m==2)
+        return 28;
+    if (m==4 || m==6 || m==9 || m==11)
+        return 30;
     
-    return false;
-}
-
-bool Day30 (int d) {
-    if (1<=d && d<=30)
-        return true;
-    
-    return false;
-}
-
-bool Day31 (int d) {
-    if (1<=d && d<=31)
-        return true;
-    
-    return false;
+    return 31;
 }
 
 bool ExistDay(int m, int d) {
-    if (m==2)
-        return Day24(d);
-    else if (1<=m && m<8) {
-        if (m%2==0)
-            return Day30(d);
-        else
-            return Day31(d);
-    }
-    else if (8<=m && m<13) {
-        if (m%2==0)
-            return Day31(d);
-        else
-            return Day30(d);
-    }
-
+    if (m<=12 && d <= LastDayNum(m))
+        return true;
+    
     return false;
 }
 
